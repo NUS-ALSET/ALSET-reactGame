cb72c5e982dc05e2cbdbdacf6f44bca4cf0b87e7
walkwel <developer.up90@gmail.com>
2018-05-02 17:25:09 +0530

    simulate_game script added

--- app/components/app/App.jsx
+++ app/components/app/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import Games from '../../../build/';
+import config from '../../config';
 
 export default class App extends React.Component {
+  getPlayersCommands(world, playerNum) {
+    try {
+      const funName = `player${playerNum}Function`;
+      let result = eval(`(${config[funName]}())`);
+      return result;
+    } catch (err) {
+      //console.log(err);
+    }
+  }
   render() {
     return (
       <div>
         <h1>Hello from component</h1>
-        <Games />
+        <Games
+          game={config.game}
+          mode={config.mode}
+          player1Function={world => this.getPlayersCommands(world, 1)}
+          player2Function={world => this.getPlayersCommands(world, 2)}
+        />
       </div>
     );
   }
