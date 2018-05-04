014e598515630f1bac63e119a32ee8085aa90f45
walkwel <developer.up90@gmail.com>
2018-05-04 17:13:45 +0530

    add players custom function via terminal

---
+++ node/util.js
@@ -0,0 +1,14 @@
+const { ROWS, COLS, GEM, LEFT, RIGHT, UP, DOWN, EMPTY } = require('./constants');
+
+const getBotDirection = (world, playerPos) => {
+  const dirs = [UP, DOWN, LEFT, RIGHT];
+  const index = Math.floor(Math.random() * 4);
+  return dirs[index];
+};
+const getRandomValue = (max = ROWS, min = 0) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+const clearConsole = () => {
+  console.log('\x1Bc');
+};
+module.exports = { getBotDirection, getRandomValue, clearConsole };
